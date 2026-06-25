ec8fc37ab4af37a0f8d7439f40573757b7639abe
aditya-narayan-dubey <uhai3582@gmail.com>
2026-06-25 19:50:23 +0530

    n

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
     iconTl.current = gsap
       .timeline({ paused: true })
       .to(topLineRef.current, {
-        rotate: 45,
+        rotate: 44,
         y: 3.3,
         duration: 0.3,
         ease: "power2.inOut",
@@ -78,7 +78,7 @@ const Navbar = () => {
       .to(
         bottomLineRef.current,
         {
-          rotate: -45,
+          rotate: -44,
           y: -3.3,
           duration: 0.3,
           ease: "power2.inOut",
